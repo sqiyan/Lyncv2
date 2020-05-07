fa00fcdcd14e00b020286f5c2776f6d4c86306b4
Qi Yan <60425964+sqiyan@users.noreply.github.com>
2020-05-07 23:35:54 +0800

    updated registration UI

---
+++ Lyncv2/PostRegistrationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostRegistrationViewController.swift
+//  Lyncv2
+//
+//  Created by Seah Qi Yan on 7/5/20.
+//  Copyright © 2020 Seah Qi Yan. All rights reserved.
+//
+
+import UIKit
+
+class PostRegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
